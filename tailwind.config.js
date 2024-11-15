6e8a2d5b02d567722c28674dcff36247ec93e04a
rijalllhd <ijal1445b@gmail.com>
2024-11-15 20:25:15 +0700

    done1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/**/*.html', 
-    "./src/**/*.{html,js}",
+    './public/**/*.{html,js}',
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
